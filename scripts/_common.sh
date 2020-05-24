af3617921d97b477c56011718d7742d9d5eaef86
yalh76 <yalh@yahoo.com>
2020-05-24 03:00:28 +0200

    various corrections

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,8 @@
 # dependencies used by the app
 pkg_dependencies="curl"
 
+swap_needed=512
+
 YNH_PHP_VERSION="7.3"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-simplexml php${YNH_PHP_VERSION}-curl"
