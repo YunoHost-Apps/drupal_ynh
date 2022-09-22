3cd31d7cee8f107ed92cf995958988e82f5a0673
yalh76 <yalh@yahoo.com>
2022-09-23 00:39:04 +0200

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,15 +3,15 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+# PHP APP SPECIFIC
+#=================================================
 
-# dependencies used by the app
-pkg_dependencies="curl"
+php_dependencies="php$YNH_DEFAULT_PHP_VERSION-fpm php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-mysql php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-ldap php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-json php$YNH_DEFAULT_PHP_VERSION-simplexml php$YNH_DEFAULT_PHP_VERSION-curl"
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="curl $php_dependencies"
 
 swap_needed=512
-
-YNH_PHP_VERSION="7.4"
-
-extra_php_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-simplexml php${YNH_PHP_VERSION}-curl"
 
 #=================================================
 # PERSONAL HELPERS
