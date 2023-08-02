d07bc626c82958133a5e2f574c0697c56cbfe9e8
yalh76 <yalh@yahoo.com>
2023-08-03 01:59:16 +0200

    Fix bookworm

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,9 @@
 # PHP APP SPECIFIC
 #=================================================
 
-YNH_PHP_VERSION=8.0
+YNH_PHP_VERSION="8.1"
 
-php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-json php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-curl"
+php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-common php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-json php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-opcache php$YNH_PHP_VERSION-readline php$YNH_PHP_VERSION-sqlite3 php$YNH_PHP_VERSION-apcu"
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="curl $php_dependencies"
