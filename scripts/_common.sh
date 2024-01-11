52bec0e3d098219a6083cd116ce37f2d3cd8ba61
Félix Piédallu <felix@piedallu.me>
2024-01-11 16:52:09 +0100

    update manifestv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,13 +6,6 @@
 # PHP APP SPECIFIC
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.1"
-
-#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-fpm php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-common php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-ldap php$YNH_PHP_VERSION-mbstring php$YNH_PHP_VERSION-json php$YNH_PHP_VERSION-simplexml php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-opcache php$YNH_PHP_VERSION-readline php$YNH_PHP_VERSION-sqlite3 php$YNH_PHP_VERSION-apcu"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="curl $php_dependencies"
-
 swap_needed=512
 
 #=================================================
