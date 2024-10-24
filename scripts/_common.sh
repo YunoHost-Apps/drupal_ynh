526839e5b222148aa9456c640b038d2ff46f7cba
jojo <jojo>
2024-10-24 13:19:03 +0000

    set common versions to php8.3 and composer2.8.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,8 @@
 #=================================================
 
 swap_needed=512
+php_version=8.3
+composer_version="2.8.1"
 
 _ynh_exec_with_drush_php() {
     ynh_hide_warnings ynh_exec_as_app \
