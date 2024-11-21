70bb9f0b74e8b88dc31d9d49ae2499e1d7fe428b
yalh76 <yalh@yahoo.com>
2024-11-21 03:46:07 +0100

    more cleanup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,9 @@
 #=================================================
 
 composer_version="2.8.3"
-
+php_version=8.2
 _ynh_exec_with_drush_php() {
-    ynh_exec_and_print_stderr_only_if_error ynh_exec_as_app \
-        env PATH="$PATH" \
+    ynh_hide_warnings ynh_exec_as_app \
+        env PATH="$PATH" DRUSH_PHP="/usr/bin/php$php_version" \
         "$@"
 }
