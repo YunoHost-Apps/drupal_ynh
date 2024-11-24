f0d0b8e75840ccf7cab34f74a11c0fd8f0050037
yalh76 <yalh@yahoo.com>
2024-11-24 06:29:23 +0100

    Fix 2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,11 +3,12 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-# PHP APP SPECIFIC
-#=================================================
+
+composer_version="2.8.3"
 
 _ynh_exec_with_drush_php() {
     ynh_hide_warnings ynh_exec_as_app \
-        env PATH="$PATH" DRUSH_PHP="/usr/bin/php$php_version" \
+        env PATH="$install_dir/vendor/bin:$PATH" \
+        DRUSH_PHP="/usr/bin/php$php_version" \
         "$@"
 }
